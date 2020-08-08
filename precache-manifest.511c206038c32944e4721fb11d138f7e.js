de37cbf08da218d10aecc1407481ad05c4774878
kentDevFrontEnd <qiang412@gmail.com>
2020-08-08 21:30:52 +0700

    Updates

---
+++ precache-manifest.511c206038c32944e4721fb11d138f7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc26e495aea6973cf686b9d2c75285f8",
+    "url": "/kentDevFrontEnd/index.html"
+  },
+  {
+    "revision": "1ae13c8029a2f036e9c3",
+    "url": "/kentDevFrontEnd/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "23f0069c023a18aaaba2",
+    "url": "/kentDevFrontEnd/static/css/main.bc51b00b.chunk.css"
+  },
+  {
+    "revision": "1ae13c8029a2f036e9c3",
+    "url": "/kentDevFrontEnd/static/js/2.f1fcacdd.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/kentDevFrontEnd/static/js/2.f1fcacdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f0069c023a18aaaba2",
+    "url": "/kentDevFrontEnd/static/js/main.8c17e13a.chunk.js"
+  },
+  {
+    "revision": "c7df2d1bcce0b1be5b07",
+    "url": "/kentDevFrontEnd/static/js/runtime-main.cd20d6a2.js"
+  }
+]);
